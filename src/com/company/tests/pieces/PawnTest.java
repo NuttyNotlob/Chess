af8763baa640d8c5401734f955b8f09f14ed2de3
LAPTOP-NN5A72M9\alex- <boltonalex89@gmail.com>
2021-03-22 23:15:35 +0000

    - Full pawn test implementation

--- src/com/company/tests/pieces/PawnTest.java
+++ src/com/company/tests/pieces/PawnTest.java
@@ -51,33 +51,95 @@ class PawnTest {
         // Test on second move
         board.getGameboard()[3][4] = null;
         board.getGameboard()[3][5] = null;
+        assertTrue(pawn.canMove(3,5));
         pawn.incrementM_timesMoved();
         assertFalse(pawn.canMove(3,5));
-
     }
 
     @Test
     void restrictedMove() {
+        // Set up the board and piece on the board
+        Board board = new Board();
+        Player white = new Player("White");
+        Pawn pawn = new Pawn(3,3,white,board);
+        board.getGameboard()[3][3] = pawn;
 
+        // Test different restricted movement
+        assertFalse(pawn.canMove(3,2));
+        assertFalse(pawn.canMove(4,4));
+        assertFalse(pawn.canMove(3,6));
     }
 
     @Test
     void captureUpLeft() {
+        // Set up the board and piece on the board
+        Board board = new Board();
+        Player white = new Player("White");
+        Player black = new Player("Black");
+        Pawn pawn = new Pawn(3,3,white,board);
+        Pawn enemyPawn = new Pawn(2,4,black,board);
+        board.getGameboard()[3][3] = pawn;
+        board.getGameboard()[2][4] = enemyPawn;
 
+        // Ensure capture works
+        assertTrue(pawn.canCapture(2,4));
     }
 
     @Test
     void captureUpRight() {
+        // Set up the board and piece on the board
+        Board board = new Board();
+        Player white = new Player("White");
+        Player black = new Player("Black");
+        Pawn pawn = new Pawn(3,3,white,board);
+        Pawn enemyPawn = new Pawn(4,4,black,board);
+        board.getGameboard()[3][3] = pawn;
+        board.getGameboard()[4][4] = enemyPawn;
 
+        // Ensure capture works
+        assertTrue(pawn.canCapture(4,4));
     }
 
     @Test
     void restrictedCapture() {
+        // Set up the board and pieces on the board
+        Board board = new Board();
+        Player white = new Player("White");
+        Player black = new Player("Black");
+        Pawn pawn = new Pawn(3,3,white,board);
+        Pawn enemyPawnOne = new Pawn(4,2,black,board);
+        Pawn enemyPawnTwo = new Pawn(2,2,black,board);
+        Pawn enemyPawnThree = new Pawn(3,4,black,board);
+        Pawn enemyPawnFour = new Pawn(5,5,black,board);
+        board.getGameboard()[3][3] = pawn;
+        board.getGameboard()[4][2] = enemyPawnOne;
+        board.getGameboard()[2][2] = enemyPawnTwo;
+        board.getGameboard()[3][4] = enemyPawnThree;
+        board.getGameboard()[5][5] = enemyPawnFour;
 
+        // Test restricted captures
+        assertFalse(pawn.canCapture(4,2));
+        assertFalse(pawn.canCapture(2,2));
+        assertFalse(pawn.canCapture(3,4));
+        assertFalse(pawn.canCapture(5,5));
+
+        // Make sure capture is working still just to check
+        Pawn enemyPawnFive = new Pawn(2,4,black,board);
+        board.getGameboard()[2][4] = enemyPawnFive;
+        assertTrue(pawn.canCapture(2,4));
     }
 
     @Test
     void captureOwnPiece() {
+        Board board = new Board();
+        Player white = new Player("White");
+        Pawn pawn = new Pawn(3,3,white,board);
+        Pawn ownPawn = new Pawn(4,4,white,board);
+        board.getGameboard()[3][3] = pawn;
+        board.getGameboard()[2][4] = ownPawn;
+
+        // Ensure capture works
+        assertFalse(pawn.canCapture(2,4));
 
     }
 
